0bdd7f15eb45ee397f481d4c00a65e232a689063
Andrés Sanabria <jandrev1312@gmail.com>
2022-10-08 21:00:47 -0500

    Added CV

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,17 @@ import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav className="mb-16 w-full py-3 text-center">
+    <nav className="relative mb-16 mt-4 w-full text-center">
       <Link href="/">
         <a className="text-xl font-bold">A. Sanabria</a>
       </Link>
+      <a
+        href="https://drive.google.com/file/d/17GrzgimnMeaFP7sfPnHsVZpmZdJtBkRJ/view?usp=sharing"
+        target="_blank"
+        className="absolute bottom-0 right-0 font-medium transition-opacity hover:opacity-70"
+      >
+        CV
+      </a>
     </nav>
   )
 }
