2ac97d796ac3bac2fc201eebecfd6762e8ba8631
Andrés Sanabria <jandrev1312@gmail.com>
2022-10-14 12:19:25 -0500

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,14 @@ import Head from "next/head"
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="59a244ab-718a-4fde-bc60-7bad842c59c4"
+          src="https://jandrev-analytics.vercel.app/umami.js"
+        ></script>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
